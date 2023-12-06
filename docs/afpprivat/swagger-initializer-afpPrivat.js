722da8ebe2bae20a4ed32bcec8ad17af1a1c511a
Hans Magnus Hallaråker <55539152+Hallaraker@users.noreply.github.com>
2023-12-06 13:58:35 +0100

    Updated swagger initializers url

--- docs/afpprivat/swagger-initializer-afpPrivat.js
+++ docs/afpprivat/swagger-initializer-afpPrivat.js
@@ -3,7 +3,7 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "apidocAfpPrivat.yaml",
+    url: "afpprivat/apidocAfpPrivat.yaml",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
